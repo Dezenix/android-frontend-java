e4f39fc6096de9597484fbf3750ca974788e4132
sekocoder <sekocoder@gmail.com>
2021-12-16 22:19:20 +0530

    commit
    ..

---
+++ Hidden-Drawer-Bar/app/src/main/java/com/example/navigation_drawer/favs.java
@@ -0,0 +1,14 @@
+package com.example.navigation_drawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class favs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favs);
+    }
+}
